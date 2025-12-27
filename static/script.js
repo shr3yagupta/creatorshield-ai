54b41bac51a2dab6e8fc38bcd76d57c3bfd783c7
Shreya Gupta <shr3yagupta.33@gmail.com>
2025-12-27 11:20:05 +0530

    Phase 3: Added static CSS & JS, connected frontend, Analyze button working

---
+++ static/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+  const analyzeBtn = document.getElementById("analyzeBtn");
+
+  analyzeBtn.onclick = ()=>{
+      alert("Analyze button working!");
+  }
+
+});
